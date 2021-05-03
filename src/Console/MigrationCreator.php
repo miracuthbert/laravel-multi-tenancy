a3e0f71457f6045ee525c80c7270e8e13e193ef5
miracuthbert <miracuthbert@gmail.com>
2021-05-04 01:02:39 +0300

    Uodate migration creator

--- src/Console/MigrationCreator.php
+++ src/Console/MigrationCreator.php
@@ -25,11 +25,16 @@ class MigrationCreator extends BaseMigrationCreator
      * Create a new migration creator instance.
      *
      * @param  \Illuminate\Filesystem\Filesystem $files
+     * @param  string  $customStubPath
      * @return void
      */
-    public function __construct(Filesystem $files)
+    public function __construct(Filesystem $files, $customStubPath)
     {
-        parent::__construct($files);
+        if (property_exists(BaseMigrationCreator::class, 'customStubPath')) {
+            parent::__construct($files, $customStubPath);
+        } else {
+            parent::__construct($files);
+        }
 
         $this->files = $files;
     }
